836605518c5b1f4f61d451ab64242c1cb30c05d2
szymonc <s@c.pl>
2019-01-19 04:54:55 +0100

    cleaning up

--- src/store/TRootStore.ts
+++ src/store/TRootStore.ts
@@ -1,62 +1,7 @@
-import { Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree";
-import { TLayout } from "./layout";
+import {Instance, SnapshotIn, SnapshotOut, types} from "mobx-state-tree";
+import {TLayout} from "./layout";
+import {TMembers} from "./TTeam";
 
-export enum Role {
-  Dev = "dev",
-  SM = "sm",
-  BA = "ba"
-}
-const TMember = types
-  .model("TMember", {
-    /*index: types.number,*/
-    name: types.maybe(types.string),
-    role: types.enumeration("Role", [Role.BA, Role.Dev, Role.SM]),
-    participation: types.maybe(types.number)
-  })
-  .actions(self => ({
-    setName(name: string) {
-      self.name = name;
-    },
-    setRole(role: Role) {
-      self.role = role;
-    },
-    setParticipation(participation: number) {
-      self.participation = participation;
-    }
-  }));
-
-export interface IMember extends Instance<typeof TMember> {}
-export interface IMemberSetSnapshotIn extends SnapshotIn<typeof TMember> {}
-
-const TMembers = types
-  .model("TMembers", {
-    members: types.array(TMember)
-  })
-  .actions(self => ({
-    addMember(Member: IMember) {
-      self.members.push(Member);
-    },
-    setMember(MemberToSet: IMember, index: number) {
-      console.log("setMeber", MemberToSet, index);
-      if (self.members[index]) {
-        self.members[index] = MemberToSet;
-      }
-    },
-    removeMember(index: number) {
-      self.members.splice(index, 1);
-    },
-    popMember() {
-      self.members.pop();
-    }
-  }))
-  .views(self => ({
-    getMember(index: number) {
-      return self.members[index];
-    }
-  }));
-
-export interface IMembers extends Instance<typeof TMembers> {}
-export interface IMembersSetSnapshotIn extends SnapshotIn<typeof TMembers> {}
 
 const TBacklogItem = types.model("TMember", {
   name: types.string,
